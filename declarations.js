49940cecfe484c9f3dc249ea0ffbd15155d179c5
helouaziz <ouazizi2code@gmail.com>
2025-02-19 12:13:56 +0100

    add declaration const

--- declarations.js
+++ declarations.js
@@ -5,6 +5,5 @@ Object.freeze(arr)
 const obj = {str:str,num:num,bool:bool,undef:undef}
 Object.freeze(obj)
 const nested = {arr:[num,undef,str],obj:{str:str,num:num,bool:bool}}
-Object.freeze(nested)
-Object.freeze(nested.obj)
+Object.freeze(nested,nested.arr,nested.obj)
 
