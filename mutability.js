6321001f51b4de2ec9a9d8c21c3acede8308fb13
helouaziz <ouazizi2code@gmail.com>
2025-02-19 13:51:35 +0100

    add mutabelity obj

--- mutability.js
+++ mutability.js
@@ -1,10 +1,9 @@
 
 const clone1 = person
 const clone2 = person
+const samePerson = person
 person.age++
 person.country = "FR"
-const samePerson = {
-  name: 'Rick',
-  age:person.age,
-  country: person.country,
-}
+samePerson.country = person.country
+samePerson.age = person.age
+
