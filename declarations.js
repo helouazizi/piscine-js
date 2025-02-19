c9de6d2439dcec270a33b1709a165ee2ba987c0f
helouaziz <ouazizi2code@gmail.com>
2025-02-19 12:06:45 +0100

    add declaration const

--- declarations.js
+++ declarations.js
@@ -1,9 +1,9 @@
 const escapeStr = "\"\`\\\/'"
 const arr =[4,"2"]
 const num = 4 , str = "2" , bool = true , undef = undefined
-Object.freeze(arr)
+
 const obj = {str:str,num:num,bool:bool,undef:undef}
-Object.freeze(obj)
+
 const nested = {arr:[num,undef,str],obj:{str:str,num:num,bool:bool}}
-Object.freeze(nested)
+Object.freeze(nested,obj,arr)
 
