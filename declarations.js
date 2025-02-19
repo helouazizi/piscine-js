8266e7e3c71e86608ba4fb1e47a35df1b89c207a
helouaziz <ouazizi2code@gmail.com>
2025-02-19 11:31:19 +0100

    add declaration const

--- declarations.js
+++ declarations.js
@@ -1,8 +1,9 @@
 const escapeStr = "\"\'\\\/'"
-const num = 21
+const num = 4
 const bool = true
 const undef = undefined
-const arr =[num,escapeStr]
-const obj = {str:escapeStr,num:num,bool:bool,undef:undefined}
+const str = "2"
+const arr =[num,str]
+const obj = {str:str,num:num,bool:bool,undef:undefined}
 const nested = {arr:arr,obj:obj}
 Object.freeze(arr,obj,nested)
