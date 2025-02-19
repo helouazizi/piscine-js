2ef3198a08b5ad6e20c1a620910192d5907cf735
helouaziz <ouazizi2code@gmail.com>
2025-02-19 11:36:43 +0100

    add declaration const

--- declarations.js
+++ declarations.js
@@ -4,6 +4,8 @@ const bool = true
 const undef = undefined
 const str = "2"
 const arr =[num,str]
+arr.push(undef)
 const obj = {str:str,num:num,bool:bool,undef:undefined}
 const nested = {arr:arr,obj:obj}
 Object.freeze(arr,obj,nested)
+console.log(arr)
