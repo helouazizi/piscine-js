d85398d40f50405cf52988a3abbc89b8c2937547
helouzizi <ouazizi2code@gmail.com>
2025-02-20 11:21:59 +0100

    add dogy tear code

--- dog-years.js
+++ dog-years.js
@@ -2,21 +2,21 @@ function dogYears(planet, dogAge) {
     const year = dogAge / 60 / 60 / 24 / 365.25
     switch (planet) {
         case "mercury":
-            return year * 7 * 0.2408467;
+            return (year * 7 * 0.2408467).toFixed(2);
         case "venus":
-            return year * 7 * 0.61519726;
+            return (year * 7 * 0.61519726).toFixed(2);
         case "mars":
-            return year * 7 * 1.8808158;
+            return (year * 7 * 1.8808158).toFixed(2);
         case "jupiter":
-            return year * 7 * 11.862615;
+            return (year * 7 * 11.862615).toFixed(2);
         case "saturn":
-            return year * 7 * 29.447498;
+            return (year * 7 * 29.447498).toFixed(2);
         case "uranus":
-            return year * 7 * 84.016846;
+            return (year * 7 * 84.016846).toFixed(2);
         case "neptune":
-            return year * 7 * 164.79132;
+            return (year * 7 * 164.79132).toFixed(2);
         case "earth":
-            return year * 7 *1;
+            return (year * 7 *1).toFixed(2);
     }
 }
 
