acf38966cca767641349999ab593593ae623ca8b
helouzizi <ouazizi2code@gmail.com>
2025-02-21 14:23:50 +0100

    add code

--- slicer.js
+++ slicer.js
@@ -1,10 +1,18 @@
 function slice(text , start, end){
+    if (start < 0 && end < 0){
+        return ""
+    }
     if (start < 0) {
+        if (end !== undefined) {
+            return ""
+        }
         start = text.length - Math.abs(start)
     }
     if (end < 0) {
         end = text.length - Math.abs(end)
     }
-    return start
+    let res = "hassan"
+    console.log(res.slice(-1,-5))
+    return text[text.length-1]
 }
-console.log(slice([5,6,5,8],-2,6))
+console.log(slice([5,6,5,8],-1,3))
