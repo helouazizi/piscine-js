38221eaca18ec614525b9d6289fc67515d145d8e
helouzizi <ouazizi2code@gmail.com>
2025-02-22 16:37:01 +0100

    add mol code

---
+++ molecules-cells.js
@@ -0,0 +1,47 @@
+function RNA(str) {
+    let res = ""
+    for (let i = 0; i < str.length; i++) {
+        switch (str[i]) {
+            case "G":
+               res += "C";
+               break
+            case "C":
+                res += "G";
+                break
+            case "T":
+               res+= "A";
+               break
+            case "A":
+                res+= "U";
+                break
+
+        }
+    }
+
+    return res
+}
+
+function DNA(str){
+    let res = ""
+    for (let i = 0; i < str.length; i++) {
+        switch (str[i]) {
+            case "C":
+               res += "G";
+               break
+            case "G":
+                res += "C";
+                break
+            case "A":
+               res+= "T";
+               break
+            case "U":
+                res+= "A";
+                break
+
+        }
+    }
+
+    return res
+}
+
+console.log(RNA("GC"))
