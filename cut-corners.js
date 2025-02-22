7af4755088b462c467073efcb19811736e45c533
helouzizi <ouazizi2code@gmail.com>
2025-02-22 13:06:04 +0100

    add sonecode

--- cut-corners.js
+++ cut-corners.js
@@ -1,92 +1,99 @@
 function round(nums) {
-    let isarray = false
-    if (Array.isArray(nums)) {
-        isarray = true
+
+    let isarray = true
+    if (!Array.isArray(nums)) {
+        nums = [nums]
+        isarray = false
     }
-    console.log(isarray,nums.length)
     let res = []
     for (let i = 0; i < nums.length; i++) {
         let num = nums[i]
-        console.log(nums[i])
         let intnum = trunc(num)
-        let diff = num - intnum
+        let diff = 0
+
+
         if (num >= 0) {
-            console.log("+")
-            if (diff < 0.5) {
+            if (diff = num - intnum < 0.5) {
                 res.push(intnum)
-            }else{
-                res.push(intnum+1)
+            } else {
+                res.push(intnum + 1)
             }
         } else {
-            console.log("-")
-            if (diff <= 0.5) {
-                res.push(intnum-1)
-            }else{
+            //console.log("yguyyu");
+
+            if (-(diff = num - intnum) < 0.5) {
                 res.push(intnum)
+            } else {
+
+                res.push(intnum - 1)
             }
-           // return diff <= -0.5 ? intnum - 1 : intnum
         }
 
-    }
-    console.log(res)
 
-    return isarray ? res : res.join()
+    }
+
+    return isarray ? res : res.join() * 1
 
 }
 
 function ceil(nums) {
+    console.log(nums);
+
     // let intnum = trunc(nums);
     // return nums === intnum ? intnum : intnum + 1;
-    let isarray = false
-    if (Array.isArray(nums)) {
-        isarray = true
+    let isarray = true
+    if (!Array.isArray(nums)) {
+        nums = [nums]
+        isarray = false
     }
     let res = []
     for (let i = 0; i < nums.length; i++) {
         let num = nums[i]
         let intnum = trunc(num)
-       // let diff = num - intnum
-        if (num === intnum) {
-                res.push(intnum)
-            //return diff < 0.5 ? intnum : intnum + 1
-        } else {
-                res.push(intnum+1)
-           // return diff <= -0.5 ? intnum - 1 : intnum
+        // let diff = num - intnum
+        if (num > 0) {
+            res.push(intnum + 1)
+        }else {
+            res.push(intnum)
         }
+
+        // return diff <= -0.5 ? intnum - 1 : intnum
+
 
     }
 
-    return isarray ? res : res.join()
+    return isarray ? res : res.join() * 1
 }
 
 function floor(nums) {
     // let intnum = trunc(nums);
     // return nums === intnum ? intnum : nums < 0 ? intnum - 1 : intnum;
-    let isarray = false
-    if (Array.isArray(nums)) {
-        isarray = true
+    let isarray = true
+    if (!Array.isArray(nums)) {
+        nums = [nums]
+        isarray = false
     }
     let res = []
     for (let i = 0; i < nums.length; i++) {
         let num = nums[i]
         let intnum = trunc(num)
-       // let diff = num - intnum
+        // let diff = num - intnum
         if (num === intnum) {
-                res.push(intnum)
+            res.push(intnum)
             //return diff < 0.5 ? intnum : intnum + 1
         } else {
-            if (num < 0){
-                res.push(intnum-1)
-            }else{
+            if (num < 0) {
+                res.push(intnum - 1)
+            } else {
                 res.push(intnum)
             }
-                
-           // return diff <= -0.5 ? intnum - 1 : intnum
+
+            // return diff <= -0.5 ? intnum - 1 : intnum
         }
 
     }
 
-    return isarray ? res : res.join()
+    return isarray ? res : res.join() * 1
 }
 
 function trunc(num) {
@@ -114,4 +121,6 @@ function trunc(num) {
 
     return isNegative ? -result : result;
 }
-console.log(round([25.32,524.25]))
+
+//console.log(round(-2.218281828459045))
+
