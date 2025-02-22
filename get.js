b8ec615597536d3c094337a51e6dfc2717873b86
helouzizi <ouazizi2code@gmail.com>
2025-02-22 16:53:41 +0100

    add geys code

---
+++ get.js
@@ -0,0 +1,15 @@
+function get(src,path){
+    let keys = path.split('.')
+    if (keys.length === 1){
+        return src[keys[0]]
+    }
+    if (keys[0] in src ){
+     return get(src[keys[0]],keys.slice(1).join('.'))
+    }
+    
+    return undefined
+}
+const src = { nested: { key: 'peekaboo' } }
+const path = 'nested.key'
+let obj = {k : "v"}
+console.log(get(src, path));
