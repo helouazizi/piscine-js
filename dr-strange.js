6f6b1b40e43d1f804cf781a4e5cd35bf4619583c
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-26 14:08:06 +0000

    ad new time

--- dr-strange.js
+++ dr-strange.js
@@ -1,6 +1,30 @@
-function addWeek(time){
+const days = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+    "secondMonday", "secondTuesday", "secondWednesday", "secondThursday",
+    "secondFriday", "secondSaturday", "secondSunday"
+]
+function addWeek(time) {
+    let times = time.split('-').map(index => Number(index))
+    let day = times[2]
+    if (day-14 <= 0) {
+        console.log( day)
+        console.log(days[day-1])
+    }else if(day-14 > 0 && day-14 <= 14 ){
+        day = day -14
+        console.log( day)
+        console.log(days[day-1])
+    }else {
+        day = day - 28 
+        console.log( day)
+        console.log(days[day-1])
+    }
+
+    console.log(times)
+
 
 }
-function timeTravel(){
-    
+function timeTravel() {
+
 }
+
+addWeek("0001-01-09")
