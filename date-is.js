0bb9ac892467b4e99eb67195b6baf85da4f7c201
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-27 10:32:43 +0000

    ad some code

--- date-is.js
+++ date-is.js
@@ -13,4 +13,16 @@ function isValid(date){
     return !isNaN(tets.getTime()) 
 }
 
-console.log(isValid(''))
+function isAfter(date,date1){
+    return date > date1
+}
+function isBefore(date,date1){
+    return date1 > date 
+}
+
+function isFuture(date){
+    return isValid(date) && isAfter(date,Date.now())
+}
+function isFuture(date){
+    return isValid(date) && isAfter(date,Date.now())
+}
