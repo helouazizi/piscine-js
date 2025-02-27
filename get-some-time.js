1f4bd6b4392280b08189f1cfaac0082dd67f8d5c
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-27 17:01:24 +0000

    hgfhghg

--- get-some-time.js
+++ get-some-time.js
@@ -1,4 +1,11 @@
-function firstDayWeek(num,year){
-    console.log(num%12)
-    return '01-01-1000'
+function firstDayWeek(num, year) {
+    year = parseInt(year);  // Ensure year is a number
+    let date = new Date(year, 0, 1);  // Start at January 1st
+
+    let offsetday = (num-1)*7
+    
+    return `${day}-${month}-${formattedYear}`;
 }
+
+console.log(firstDayWeek(1, '1000'));  // Expected: "01-01-1000"
+
