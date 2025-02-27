93f47bfb44c6a07715bd4182e38daa0b12a51c88
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-27 18:19:44 +0000

    add some

--- using-map.js
+++ using-map.js
@@ -1,16 +1,37 @@
 function citiesOnly(arr){
-    return arr.map(index => index[city])
+    return arr.map(index => index['city'])
+}
+function upperCasingStates(arr){
+    return arr.map(word =>{
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        });
+       // return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+
 }
 
+function fahrenheitToCelsius (arr){
+    return arr.map(temp => {
+        return Math.round((parseFloat(temp)-32)*(5/9))+'°C'
+    })
+}
+
+function trimTemp(arr){
+    return arr.map(obj => ({
+         ...obj,
+
+         temperature : obj.temperature.replace(/\s+/g,'')
+    }))
+}
+
+function tempForecasts(arr){
+    return arr.map(obj => obj.temperature.replace(/\s+/g,'')+'Celsius'+' in '+ obj.city+', '+obj.state)
+}
 let test =[
     {
-      city: 'Los Angeles',
-      temperature: '  101 °F   ',
-    },
-    {
-      city: 'San Francisco',
-      temperature: ' 84 ° F   ',
+      city: 'Pasadena',
+      temperature: ' 101 °F',
+      state: 'california',
+      region: 'West',
     },
   ]
-
-  console.log(citiesOnly(test))
+  console.log(tempForecasts(test))
