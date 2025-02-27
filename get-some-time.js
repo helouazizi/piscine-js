3e8125688710692468c2625ed77189eb2ed16588
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-27 17:27:27 +0000

    hgfhghg

--- get-some-time.js
+++ get-some-time.js
@@ -3,9 +3,13 @@ function firstDayWeek(num, year) {
     let date = new Date(year, 0, 1);  // Start at January 1st
 
     let offsetday = (num-1)*7
-    
-    return `${day}-${month}-${formattedYear}`;
+    date.setDate(date.getDate()+offsetday)
+    let day = String(date.getDate()).padStart(2,'0')
+    year = String(year).padStart(4,'0')
+    let month = String(date.getMonth()+1).padStart(2,'0')
+
+    return `${day}-${month}-${year}`;
 }
 
-console.log(firstDayWeek(1, '1000'));  // Expected: "01-01-1000"
+console.log(firstDayWeek(52, '1000'));  // Expected: "01-01-1000"
 
