9afc3e05b04be956c9c999be700025ff990fef91
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-28 16:01:34 +0000

    add sume everey

--- long-words.js
+++ long-words.js
@@ -1,3 +1,12 @@
 function longWords(arr){
-    
+    return arr.every(word => typeof word === 'string' && word.length >= 5)
 }
+function oneLongWord(arr){
+    return arr.some(word => typeof word === 'string' && word.length >= 10)
+}
+function noLongWords(arr){
+    return !arr.some(word => typeof word === 'string' && word.length >= 7)
+}
+const words = [ 5, 'elite', 'exuberant', 'destruction', 'present']
+
+console.log(longWords(words))
