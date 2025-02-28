7dafc80b2574f35e45c339d0ea4f04cf09d75c2c
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-28 13:55:48 +0000

    ad abs

--- recode/abs.js
+++ recode/abs.js
@@ -1,3 +1,20 @@
-function isPositive(num){
-    
+function isPositive(num) {
+    if (Object.is(num, 0)) {
+        return true
+    } else if (Object.is(num, -0)) {
+        return false
+    } else if (num < 0) {
+        return false
+    }else{
+        return true
+    }
 }
+
+function abs(num){
+    if (!isPositive(num)){
+        return -num
+    }
+    return num
+}
+
+console.log(abs(8))
