56d6276ea337db9b9784172bf15627a058b415ad
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-28 14:30:50 +0000

    add some magic

--- recode/is.js
+++ recode/is.js
@@ -1,12 +1,40 @@
 const is = {}
 
-is.hassan = function (text){
-    return text === "hassan"
-}
 is.num = function (num){
    return typeof num === 'number'
 }
 
+is.nan = function(nan){
+    return Number.isNaN(nan)
+}
+
+is.str = function (str){
+    return typeof  str === 'string'
+}
+is.bool = function (str){
+    return typeof  str === 'boolean'
+}
+is.undef = function (str){
+    return typeof  str === 'undefined'
+}
+is.def = function (str){
+    return typeof  str === 'defined'
+}
+is.arr = function (str){
+    return Array.isArray(str)
+}
+is.obj = function (value){
+    return value !== null && typeof value === 'object' && !is.arr(value);
+}
+is.func = function (str){
+    return typeof  str === 'function' 
+}
+is.truthy = function (str){
+    return typeof  str === 'truthy' 
+}
+is.falsy = function (str){
+    return typeof  str === 'falsy' 
+}
 
 
-console.log(is.num("5"))
+console.log(typeof(Object.is))
