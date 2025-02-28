7072395a8c257fc2d4552c20943b883621ed6c15
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-28 13:08:47 +0000

    recode change

---
+++ recode/change.js
@@ -0,0 +1,13 @@
+// const sourceObject = {
+//     num: 42,
+//     bool: true,
+//     str: 'some text',
+//     log: console.log,
+//   }
+
+  const get = (key) => sourceObject[key]
+  const set = (key,val) => sourceObject[key] = val
+
+
+//   console.log(set("num",6))
+//   console.log(sourceObject)
