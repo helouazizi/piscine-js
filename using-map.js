8dd613b45b4dac9e6bc28577b68ce9d35ed4afb6
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-28 10:16:56 +0000

    add map functions

--- using-map.js
+++ using-map.js
@@ -2,36 +2,24 @@ function citiesOnly(arr){
     return arr.map(index => index['city'])
 }
 function upperCasingStates(arr){
-    return arr.map(word =>{
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-        });
-       // return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-
-}
+    return arr.map(word =>word.split(' ').map(kalma =>kalma.charAt(0).toUpperCase()+ kalma.slice(1).toLowerCase()).join(' '))}
 
 function fahrenheitToCelsius (arr){
     return arr.map(temp => {
-        return Math.round((parseFloat(temp)-32)*(5/9))+'°C'
+        return Math.floor((parseFloat(temp)-32)*(5/9))+'°C'
     })
 }
 
 function trimTemp(arr){
     return arr.map(obj => ({
          ...obj,
-
          temperature : obj.temperature.replace(/\s+/g,'')
     }))
 }
 
 function tempForecasts(arr){
-    return arr.map(obj => obj.temperature.replace(/\s+/g,'')+'Celsius'+' in '+ obj.city+', '+obj.state)
+    return arr.map(obj => fahrenheitToCelsius([obj.temperature.replace(/\s+/g,'')])+'elsius'+' in '+ upperCasingStates([obj.city])+', '+upperCasingStates([obj.state]))
 }
-let test =[
-    {
-      city: 'Pasadena',
-      temperature: ' 101 °F',
-      state: 'california',
-      region: 'West',
-    },
-  ]
-  console.log(tempForecasts(test))
+
+let text = ['alabama', 'new jersey']
+console.log(upperCasingStates(text))
