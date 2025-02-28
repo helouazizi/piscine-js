b9a3e2fec53834b6bcff40df6b64902e4e97c0bd
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-28 12:54:47 +0000

    add mutability

--- recode/mutability.js
+++ recode/mutability.js
@@ -1,10 +1,12 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-  }
-
+// const person = {
+//     name: 'Rick',
+//     age: 77,
+//     country: 'US',
+//   }
   const clone1 =  {...person} 
+  const clone2 = {...person}
+  const samePerson = person
   person.age++
+  person.country = 'FR'
 
-  console.log(person,clone1)
+  //console.log(person,clone1,samePerson)
