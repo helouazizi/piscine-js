0d68d933b311859e068bcfa9457a5d9ddf39d5bd
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-28 14:12:58 +0000

    add  num function

--- recode/is.js
+++ recode/is.js
@@ -3,5 +3,10 @@ const is = {}
 is.hassan = function (text){
     return text === "hassan"
 }
+is.num = function (num){
+   return typeof num === 'number'
+}
 
-console.log(is.hassan("HASSAN"))
+
+
+console.log(is.num("5"))
