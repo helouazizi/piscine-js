e96daf6cca7a2a056bbc697505ee978b1126df7b
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-01 10:33:06 +0000

    add somme

--- quantifiers.js
+++ quantifiers.js
@@ -6,14 +6,14 @@ function every(arr, func){
    } 
    return true
 }
-// function some(arr, func){
-//     for(let i = 0 ; i < arr.length ; i++){
-//          if(func(arr[i],i,arr)){
-//              return true
-//          }
-//     } 
-//     return false
-//  }
+function some(arr, func){
+    for(let i = 0 ; i < arr.length ; i++){
+         if(func(arr[i],i,arr)){
+             return true
+         }
+    } 
+    return false
+ }
 //  function none(arr, func){
 //     for(let i = 0 ; i < arr.length ; i++){
 //          if(func(arr[i],i,arr)){
