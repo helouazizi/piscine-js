b379f29800333c2000a78ee1a623ae9773f15734
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-01 10:00:47 +0000

    add reject

---
+++ filter.js
@@ -0,0 +1,18 @@
+function filter(arr, func) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i],i,arr)){
+            res.push(arr[i])
+        }
+    }
+    return res
+}
+function reject(arr, func) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!func(arr[i],i,arr)){
+            res.push(arr[i])
+        }
+    }
+    return res
+}
