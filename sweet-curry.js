9cc363d69611d784d8f54a2d29119cf8322f8900
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-01 10:41:14 +0000

    add add3

--- sweet-curry.js
+++ sweet-curry.js
@@ -4,13 +4,13 @@ function mult2(a){
     }
 }
 
-// function add3(a){
-//     return function(b){
-//         return function(c){
-//             return a+b+c
-//         }
-//     }
-// }
+function add3(a){
+    return function(b){
+        return function(c){
+            return a+b+c
+        }
+    }
+}
 
 // function sub4(a){
 //     return function(b){
