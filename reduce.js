b0248fe1edd712461e1c5cbaaf8a218f857c8702
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-01 10:20:00 +0000

    asgfsd

--- reduce.js
+++ reduce.js
@@ -1,13 +1,26 @@
-function fold(arr,func,accum=0){
-   for  (let i =0; i < arr.length ; i++){
-    accum = func(accum,arr[i])
-   }
-   return accum
-}
-
-function foldRight(arr,func,accum=0){
-    for  (let i = arr.length-1 ; i>=0; i--){
-     accum = func(accum,arr[i])
+function fold(arr, func, accum = 0) {
+    for (let i = 0; i < arr.length; i++) {
+        accum = func(accum, arr[i])
     }
     return accum
- }
+}
+
+function foldRight(arr, func, accum = 0) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        accum = func(accum, arr[i])
+    }
+    return accum
+}
+function reduce(arr, func, accum = undefined) {
+    if (accum === undefined){
+        accum = arr[0]
+    }
+    if (accum === NaN){
+        return 'error'
+    }
+    for (let i = 0; i < arr.length; i++) {
+        accum = func(accum, arr[i])
+    }
+    return accum
+}
+
