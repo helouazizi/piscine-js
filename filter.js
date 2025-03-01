9472c601e0751be1a5cbc35703158f2bc6121dfe
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-01 10:03:33 +0000

    asfdsdgf

--- filter.js
+++ filter.js
@@ -16,3 +16,14 @@ function reject(arr, func) {
     }
     return res
 }
+function partition(arr, func) {
+    let res = [[],[]]
+    for (let i = 0; i < arr.length; i++) {
+        if (!func(arr[i],i,arr)){
+            res[0].push(arr[i])
+        }else{
+            res[1].push(arr[i])
+        }
+    }
+    return res
+}
