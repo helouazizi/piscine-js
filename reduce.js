7907d57146b64d35e0bf6ee4d3b9bb9a4dab44ae
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-01 10:26:16 +0000

    add reduce rigth

--- reduce.js
+++ reduce.js
@@ -22,5 +22,16 @@ function reduce(array, func) {
     }
     return accum;
 }
+function reduceRight(array, func) {
+    if (array.length < 1) {
+        throw new Error("Reduce of empty array with no initial value");
+    }
+
+    let accum = array[array.length-1]
+    for (let i =array.length-2; i >=0; i--) {
+        accum = func(accum, array[i])
+    }
+    return accum;
+}
 
 
