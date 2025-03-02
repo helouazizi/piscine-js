8ae55ffc5865e855a93a3246d5f7d97d9d21940b
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-02 11:49:30 +0000

    afrsdafsd

---
+++ keycodes-symphony.js
@@ -0,0 +1,31 @@
+export const compose = () => {
+    document.addEventListener("keydown", (event) => {
+        console.log(event.key);
+
+        const container = document.body;
+
+        // lets check the keys pressed
+        if (/^[a-z]$/.test(event.key)) {
+            let note = document.createElement('div')
+            note.classList.add("note")
+            note.textContent = event.key
+            note.style.background = 'red'
+            container.append(note)
+        }
+
+        else if (event.key === 'Backspace') {
+            let notes = document.querySelectorAll('div')
+            if (notes.length > 0) {
+                notes[notes.length - 1].remove()
+            }
+
+        }
+
+        else if (event.key === 'Escape'){
+            let notes = container.querySelectorAll('.note')
+            notes.forEach(note => {
+                note.remove()
+            })
+        }
+    });
+};
