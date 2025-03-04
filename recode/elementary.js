2a37f7264192dfd9c25f4ec7afc475cc0dde5b17
helouazizi <ouazizi2code@gmail.com>
2025-03-04 22:30:08 +0000

    add

--- recode/elementary.js
+++ recode/elementary.js
@@ -23,27 +23,28 @@ function multiply(a,b){
 
 
 function divide(a,b){
+    if (Object.is(b,-0)){
+        return Number.NEGATIVE_INFINITY
+    }
+    if (Object.is(b,0)){
+        return Number.POSITIVE_INFINITY
+    }
     let res = 0
-    let sighn = false
-    if (b< 0){
-        b =-b
-        sighn = true
-    }
-    else if (a < 0){
-        sighn = true
-        a =-a
-    }
-    else if (a< 0 && b < 0){
-        sighn = false
-        a = -a
-        b = -b
-    }
+    let sighn = (a<0 !== b<0)
+    
+    a = Math.abs(a)
+    b = Math.abs(b)
     while (multiply(res,b) < a){
+        
         res++
     }
     //console.log(sighn)
-    return sighn ? -res : res
+    return sighn ? -(res-1) : (res-1)
 }
-console.log(-10/-5)
 
-console.log(divide(-10,-5))
+function modulo(a,b){
+    return a - multiply(divide(a,b),b)
+}
+//console.log(-10/-5)
+
+console.log(divide(34, 78))
