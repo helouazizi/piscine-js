6a0ec1a6355ac116d695ca6fc56d1fc4dc5e89d0
helouazizi <ouazizi2code@gmail.com>
2025-03-04 22:37:16 +0000

    add silcer function

--- recode/slicer.js
+++ recode/slicer.js
@@ -1,7 +1,12 @@
-function slice(arr,start,end){
-    if (start === undefined && end === undefined){
-       return arr
+function slice(arr, start, end = arr.length) {
+    if (start === undefined && end === undefined) {
+        return arr
     }
+    let res = []
+    for (let i = start; i < end;i++){
+        res.push(arr[i])
+    }
+    return Array.isArray(arr) ? res : res.join('')
 }
 
-console.log(slice([5,5,5,5,5,5]))
+console.log(slice("",2))
