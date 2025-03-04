b8f29fb4353ae2834f4412c7f7ef1730d9acb896
helouazizi <ouazizi2code@gmail.com>
2025-03-04 22:46:18 +0000

    add some test

--- recode/slicer.js
+++ recode/slicer.js
@@ -1,12 +1,16 @@
-function slice(arr, start, end = arr.length) {
-    if (start === undefined && end === undefined) {
-        return arr
-    }
+function slice(arr, start=0, end = arr.length) {
     let res = []
+    if (end < 0){
+        end = arr.length+end
+    }
+    if (start < 0){
+        start =arr.length + start
+    }
     for (let i = start; i < end;i++){
         res.push(arr[i])
     }
     return Array.isArray(arr) ? res : res.join('')
 }
+//console.log([5, 5, 5, 5, 5, 5].slice(-1))
 
-console.log(slice("",2))
+console.log(slice([5, 5, 5, 5, 5, 5],-1))
