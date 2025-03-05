8d49073fcd7c58e7b899656eb5e9af9784d680e5
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-05 13:50:47 +0000

    add for each

--- fusion.js
+++ fusion.js
@@ -1,3 +1,22 @@
 function fusion(...args){
-    
+    let res = {}
+    let exist = new Set()
+    args.forEach(obj => {
+        for (let key in obj){
+            if (!exist.has(key)){
+                console.log(obj[key]);
+                res[key] = 
+            }else {
+                //console.log(obj[key]);
+                
+                res[key].push(obj[key])
+            }
+           
+        }
+    })
+
+    return res
 }
+//const obj = 
+
+console.log(fusion({ arr: [1, "2"] }, { arr: [2] }))
