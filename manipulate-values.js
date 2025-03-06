cc3a29e57988c3959cc9904f7af671e7bd786911
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-06 10:30:33 +0000

    add reduceValues

--- manipulate-values.js
+++ manipulate-values.js
@@ -8,9 +8,26 @@ function filterValues(obj, func) {
     return res
 }
 
-function mapValues(obj,func){
-    
+function mapValues(obj, func) {
+    let res = {}
+    for (let key in obj) {
+
+        res[key] = func(obj[key])
+
+    }
+    return res
 }
 
-// const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
+function reduceValues(obj,func,dlm=0){
+    let res = dlm
+    for (let key in obj) {
+        res = func(res,obj[key])
+        //console.log(res);
+        
+    }
+    return res
+}
+
+ const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
 // console.log(filterValues(nutrients, (nutrient) => nutrient <= 12))
+console.log(reduceValues({ a: 1, b: 2, c: 3 }, (acc, cr) => acc + cr, 3))
