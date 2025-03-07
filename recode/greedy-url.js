02107ec546a5f5928cb2ac4b0118deabe5c1552d
helouazizi <ouazizi2code@gmail.com>
2025-03-07 20:36:46 +0000

    add greedy url

--- recode/greedy-url.js
+++ recode/greedy-url.js
@@ -1,5 +1,16 @@
 function getURL(data){
     const urlRegex = /(https?:\/\/[^\s"']+)/g;
-    return dataSet.match(urlRegex) || [];
+    return data.match(urlRegex) || [];
+}
 
+function greedyQuery(data) {
+    const queryRegex = /(https?:\/\/[^\s"']+\?([^#\s&=]+=[^#\s&=]+&){2,}[^#\s"']+)/g;
+    return data.match(queryRegex) || [];
+}
+
+function notSoGreedy(data) {
+    const queryRegex = /(https?:\/\/[^\s"']+\?([^#\s"']+=[^#\s"']+&){1,2}[^#\s"']+=[^#\s"']*)/g;
+
+
+    return data.match(queryRegex) || [];
 }
