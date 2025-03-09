cfd3038d6c459595bc72e91f7e292b8de7cf09e3
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-09 14:44:41 +0000

    dfhgdf

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,27 +1,24 @@
 function retry(count, callback) {
+    let times = 0
+    return async function test (...args) {
+        // if (count === 0) {
+        //     return await callback(...args);
+        // }
 
-    return async function (...args) {
-        if (count === 0) {
-            return await callback(...args);
-        }
-        let times = 0
-        let result;
-        while (times < count) {
-            try {
-              result = await callback(...args)
-            } catch (error) {
-                times++
-                if (times === count) {
-                    // If retries exceeded, throw an error
-                    console.log(error);
-                    
-                    throw error
-                    
-                }
-                
+        // while (times < count) {
+        try {
+            return await callback(...args)
+        } catch (error) {
+            times++
+            if (times <= count) {
+                return await test(...args)
+            } else {
+                throw error
             }
+
         }
-        return result
     }
+
+    // }
 
 }
