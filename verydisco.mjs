c42fce1f8b0bdf9049e366190627a6f9150e19ff
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-13 10:37:06 +0000

    add final fix

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,9 +1,16 @@
-import { argv } from 'node:process'
-const arg = argv.slice(2).join(" ")
-console.log(arg);
+import { argv } from "node:process";
 
-const midle = Math.ceil(arg.length / 2)
-const last = arg.slice(midle)
-const first =  arg.slice(0,midle)
-const result =last.trim()+first.trim()
+const arg = argv[2]
+
+if (!arg) {
+  console.error("Error: No input provided.");
+  process.exit(1);
+}
+
+const veryDisco = (word) => {
+  const middle = Math.ceil(word.length / 2);
+  return word.slice(middle) + word.slice(0, middle);
+};
+
+const result = arg.split(" ").map(veryDisco).join(" ");
 console.log(result);
