991f0da658260fac96250c583dc8095e31e7e3bd
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-13 10:08:47 +0000

    split the input

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,7 +1,11 @@
 import  {argv}  from 'node:process'
 
-argv.forEach(element => {
-    console.log(element);
+// argv.forEach(element => {
+//     console.log(element);
     
-});
+// });
 //
+const arg = argv.slice(2).join(" ")
+const midle = Math.ceil( arg.length / 2)
+const result = arg.slice(midle).join() + arg.slice(0,midle).join()
+console.log(result);
