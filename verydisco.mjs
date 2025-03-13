38ba41ed7903650e717cf422def9ced07e96ab31
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-13 10:09:01 +0000

    remoe foor oop

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,11 +1,5 @@
-import  {argv}  from 'node:process'
-
-// argv.forEach(element => {
-//     console.log(element);
-    
-// });
-//
+import { argv } from 'node:process'
 const arg = argv.slice(2).join(" ")
-const midle = Math.ceil( arg.length / 2)
-const result = arg.slice(midle).join() + arg.slice(0,midle).join()
+const midle = Math.ceil(arg.length / 2)
+const result = arg.slice(midle).join() + arg.slice(0, midle).join()
 console.log(result);
