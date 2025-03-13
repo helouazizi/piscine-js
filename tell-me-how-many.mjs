ab8dfba7cf60619c8bbe19902e458bafd803d287
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-13 12:58:53 +0000

    add

--- tell-me-how-many.mjs
+++ tell-me-how-many.mjs
@@ -1,15 +1,19 @@
 import { readdir } from 'node:fs/promises';
-//import { dirname } from 'node:path';
 
-async function countDirectoryEntries() {
-        const directory = process.argv[2] || process.cwd();
-        
+async function read() {
+    const directory = process.argv[2] || process.cwd();  // Get the directory from argv or default to cwd
+
+    try {
         // Read directory contents
-         return  await readdir(directory);
+        const files = await readdir(directory);
+        // files.forEach(file => {
+        //     console.log(file);  // Print each file name
+        // });
+        console.log(files.length);
+        
+    } catch (error) {
+        console.error("Error reading directory:", error.message);  // Handle errors (e.g., directory not found)
+    }
 }
 
-// Run the main function
-countDirectoryEntries().then(files => {
-    files.forEach(file => {console.log(file);
-    })
-});
+read();
