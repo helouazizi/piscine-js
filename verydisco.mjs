caaeece31888e0849b16ed83e2fb419a92178dc7
hassan el ouazizi <ouazizi2code@gmail.com>
2025-03-13 10:24:09 +0000

    add final

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,5 +1,9 @@
 import { argv } from 'node:process'
 const arg = argv.slice(2).join(" ")
+console.log(arg);
+
 const midle = Math.ceil(arg.length / 2)
-const result = arg.slice(midle).join() + arg.slice(0, midle).join()
+const last = arg.slice(midle)
+const first =  arg.slice(0,midle)
+const result =last.trim()+first.trim()
 console.log(result);
