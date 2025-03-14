4295d2a65bd49a6f9b3328290581c31af914a4bc
helouazizi <ouazizi2code@gmail.com>
2025-03-14 12:17:12 +0000

    add all function of is valid

--- recode/date-is.js
+++ recode/date-is.js
@@ -2,10 +2,20 @@ function isValid(date) {
     if (date instanceof Date) {
         return !isNaN(date.getTime())
     }
-    return typeof date === "number" && date > 0
+    return typeof date === "number" && typeof date !== "undefined" && date > 0
 }
 
-function isAfter(date) {
-    
+function isAfter(date1, date2) {
+    return date1 - date2 > 0 && isValid(date1) && isValid(date2)
 }
-console.log(isValid(Date.now()));
+
+function isBefore(date1, date2) {
+    return date1 - date2 < 0 && isValid(date1) && isValid(date2)
+}
+function isFuture(date) {
+    return date - Date.now() > 0 && isValid(date) 
+}
+function isPast(date) {
+    return date - Date.now() < 0 && isValid(date) 
+}
+console.log(isBefore(''));
