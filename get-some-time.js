cf67690d8f65b2af0b7b6fc0936db861ff06419d
helouazizi <ouazizi2code@gmail.com>
2025-03-14 15:48:42 +0000

    add get some time

--- get-some-time.js
+++ get-some-time.js
@@ -2,20 +2,16 @@ function firstDayWeek(num, year) {
     //year = parseInt(year);  // Ensure year is a number
     let date = new Date("01-01-0001");  // Start at January 1st
     date.setFullYear(year)
-    console.log(date.getFullYear() );
-    console.log(year);
-    let offsetday = (num-1)*7
-    date.setDate(date.getDate()+offsetday)
-    while (date.getDay() != 1 && date.getDate()!=1){
-        date.setDate(date.getDate()-1)
-    console.log(date.getFullYear() );
-
+    let offsetday = (num - 1) * 7
+    date.setDate(date.getDate() + offsetday)
+    while (date.getDay() != 1 && date.getDate() != 1) {
+        date.setDate(date.getDate() - 1)
+        console.log(date.getFullYear());
         console.log("hjhjh");
-        
-    } 
-    let day = String(date.getDate()).padStart(2,'0')
-    year = String(year).padStart(4,'0')
-    let month = String(date.getMonth()+1).padStart(2,'0')
+    }
+    let day = String(date.getDate()).padStart(2, '0')
+    year = String(year).padStart(4, '0')
+    let month = String(date.getMonth() + 1).padStart(2, '0')
 
     return `${day}-${month}-${year}`;
 }
--- recode/get-some-time.js
+++ recode/get-some-time.js
@@ -1,3 +1,15 @@
 function firstDayWeek(num,year) {
-    
+    let dayofset = ((num-1) *7)
+    let date = new Date("01-01-0001");
+    date.setFullYear(year)
+    date.setDate(date.getDate()+dayofset)
+    while (date.getDate() !== 1 && date.getDay()!== 1){
+        date.setDate(date.getDate()-1)
+    }
+    console.log(date);
+    let day = String(date.getDate()).padStart(2,'0')
+    let moth = String(date.getMonth()+1).padStart(2,'0')
+    let finalyear = String(date.getFullYear()).padStart(4,'0')
+    return `${day}-${moth}-${finalyear}`  
 }
+console.log(firstDayWeek(52, '1000'));
