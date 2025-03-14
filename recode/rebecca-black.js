1ad625f7122d4d7cadaf56d2160beadb0acd2357
helouazizi <ouazizi2code@gmail.com>
2025-03-14 19:48:29 +0000

    add getFullYear

--- recode/rebecca-black.js
+++ recode/rebecca-black.js
@@ -1,3 +1,13 @@
 function isFriday(date) {
+    return date.getDay() === 5
+}
+
+
+function isWeekend(date) {
+    return date.getDay() === 6 || date.getDay() === 0
+}
+
+
+function isLeapYear(date) {
     
 }
