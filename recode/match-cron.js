f11e28b9dce147b01f9e5883a4c143ce9013293c
helouazizi <ouazizi2code@gmail.com>
2025-03-14 20:27:26 +0000

    add some

---
+++ recode/match-cron.js
@@ -0,0 +1,22 @@
+export function matchCron(patern, date) {
+    const [min, hour, datee, month, day] = patern.split(' ');
+    
+    const parse = (val) => val === '*' ? '*' : parseInt(val, 10);
+
+    const cronvalues = [
+        parse(min),
+        parse(hour),
+        parse(datee),
+        parse(month),
+        parse(day),
+    ];
+
+    const datevalues = [
+        date.getMinutes(),
+        date.getHours(),
+        date.getDate(),
+        date.getMonth() + 1,
+        date.getDay() === 0 ? 7 : date.getDay(),
+    ];
+    return cronvalues.every((val, i) => val === '*' || val === datevalues[i]);
+}
