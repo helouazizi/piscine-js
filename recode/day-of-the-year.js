aaa2fe9b3b8b061ccdfe63b6d88fac8ec8e73fae
helouazizi <ouazizi2code@gmail.com>
2025-03-15 13:24:08 +0000

    use utc format

--- recode/day-of-the-year.js
+++ recode/day-of-the-year.js
@@ -1,6 +1,6 @@
 function dayOfTheYear(date) {
-    const startOfYear = Date.UTC(date.getUTCFullYear(), 0, 1); // First day of the year in UTC
-    const currentTime = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()); // Current date in UTC
+    const startOfYear = Date.UTC(date.getUTCFullYear(), 0, 1); 
+    const currentTime = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()); 
     return Math.floor((currentTime - startOfYear) / (1000 * 60 * 60 * 24)) + 1;
 }
 
