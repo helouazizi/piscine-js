6e9f6c33bb4196be36081d1dc1a6c84143bcc8fa
helouazizi <ouazizi2code@gmail.com>
2025-03-15 13:23:40 +0000

    remove some code

--- recode/day-of-the-year.js
+++ recode/day-of-the-year.js
@@ -4,9 +4,4 @@ function dayOfTheYear(date) {
     return Math.floor((currentTime - startOfYear) / (1000 * 60 * 60 * 24)) + 1;
 }
 
-console.log(dayOfTheYear(new Date('0001-01-01')) === 1);     // ✅ True
-console.log(dayOfTheYear(new Date('1664-08-09')) === 222);   // ✅ True
-console.log(dayOfTheYear(new Date('1600-12-31')) === 366);   // ✅ True (Leap Year)
-console.log(dayOfTheYear(new Date('2020-06-22')) === 174);   // ✅ True
-console.log(dayOfTheYear(new Date('2048-12-08')) === 343);   // ✅ True (Leap Year)
-console.log(dayOfTheYear(new Date('2048-11-08')) === 313);   // ✅ True (Leap Year)
+
