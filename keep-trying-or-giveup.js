08a26e51b4d1601948a1c0e346454d8af0383574
helouazizi <ouazizi2code@gmail.com>
2025-03-16 12:34:37 +0000

    add keep

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,24 +1,16 @@
 function retry(count, callback) {
-    let times = 0
-    return async function test (...args) {
-        // if (count === 0) {
-        //     return await callback(...args);
-        // }
-
-        // while (times < count) {
-        try {
-            return await callback(...args)
-        } catch (error) {
-            times++
-            if (times <= count) {
-                return await test(...args)
-            } else {
-                throw error
+    return async function (...args) {
+        let attempts = 0;
+        while (attempts <= count) {
+            try {
+                return await callback(...args);
+            } catch (error) {
+                attempts++;
+                if (attempts > count) {
+                    throw error;
+                }
             }
-
         }
-    }
-
-    // }
-
+    };
 }
+
