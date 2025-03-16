34f35d30666d33c1dfec44fc3c4d5f900b18cd37
helouazizi <ouazizi2code@gmail.com>
2025-03-16 13:19:20 +0000

    add fetch

--- get-json.js
+++ get-json.js
@@ -1,25 +1,13 @@
 async function getJSON(path, params) {
-    const url = new URL(path, window.location.origin);
-    
-    // Append query parameters if any
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+    const query = new URLSearchParams(params).toString();
+    const url = query ? `${path}?${query}` : path;
 
-    try {
-        const response = await fetch(url+p);
-        
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-        
-        const json = await response.json();
-        
-        if (json.error) {
-            throw new Error(json.error);
-        }
-        
-        return json.data;
-    } catch (error) {
-        throw new Error(`Fetch failed: ${error.message}`);
-    }
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(response.statusText);
+
+    const json = await response.json();
+    if (json.error) throw new Error(json.error);
+
+    return json.data;
 
 }
